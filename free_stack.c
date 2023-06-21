4d0cfd0d46e6d793f60b27bfec4bb36a65a3d6ec
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-21 04:29:11 -0700

    Update free_stack.c

--- free_stack.c
+++ free_stack.c
@@ -1,8 +1,22 @@
 #include "monty.h"
+
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+
 /**
-* free_stack - frees a doubly linked list
-* @head: head of the stack
+* free_stack - function that frees a doubly linked list.
+* @head: head of the stacks.
 */
+
 void free_stack(stack_t *head)
 {
 	stack_t *aux;
