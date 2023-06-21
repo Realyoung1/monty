e4a303f2daf9376a61f69412cf9314a3b608d3cb
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-21 04:05:20 -0700

    Update monty.h

--- monty.h
+++ monty.h
@@ -1,5 +1,17 @@
 #ifndef MONTY_H
 #define MONTY_H
+
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
