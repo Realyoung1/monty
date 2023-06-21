e1061015b4ed5d30149d00053bda8b45be14b39e
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-21 04:34:18 -0700

    Update nop.c

--- nop.c
+++ nop.c
@@ -1,10 +1,24 @@
 #include "monty.h"
+
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+
 /**
   *f_nop- nothing
   *@head: stack head
   *@counter: line_number
   *Return: no return
  */
+
 void f_nop(stack_t **head, unsigned int counter)
 {
 	(void) counter;
