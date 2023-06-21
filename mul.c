d8824ea6fdd0273528106c7430b25cbfea0dd09a
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-21 04:33:41 -0700

    Update mul.c

--- mul.c
+++ mul.c
@@ -1,10 +1,24 @@
 #include "monty.h"
+
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+
 /**
- * f_mul - multiplies the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
+ * f_mul - this multiply the top two elements of the stacks
+ * @head: stack heads.
+ * @counter: line_numbers.
+ * Return: no returns.
 */
+
 void f_mul(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
