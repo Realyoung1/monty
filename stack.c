b39c1ac35ccd1d90cd5830df740fc77a49a39652
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-21 04:51:01 -0700

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,10 +1,24 @@
 #include "monty.h"
+
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+
 /**
- * f_stack - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
+ * f_stack - prints the tops.
+ * @head: stack heads.
+ * @counter: line_numbers.
+ * Return: no returns.
 */
+
 void f_stack(stack_t **head, unsigned int counter)
 {
 	(void)head;
