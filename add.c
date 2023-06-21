ccf6b0f33100c5439ca23f232fa01933428864fe
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-21 04:19:43 -0700

    Update add.c

--- add.c
+++ add.c
@@ -1,10 +1,24 @@
 #include "monty.h"
+
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+
 /**
- * f_add - adds the top two elements of the stack.
- * @head: stack head
- * @counter: line_number
- * Return: no return
+ * f_add - func to adds the top two elements of the stack
+ * @head: the stacks head.
+ * @counter: line_numbers.
+ * Return: no returns.
 */
+
 void f_add(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
