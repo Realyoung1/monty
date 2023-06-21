6c58e08783746841646ac976a839cdfb20be998a
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2023-06-21 04:22:40 -0700

    Update addnode.c

--- addnode.c
+++ addnode.c
@@ -1,11 +1,25 @@
 #include "monty.h"
+
+#include <stdbool.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+
 /**
- * addnode - add node to the head stack
- * @head: head of the stack
- * @n: new_value
- * Return: no return
+ * addnode - func to add node to the head stacks.
+ * @head: this is the head of the stacks.
+ * @n: new_values.
+ * Return: no returns.
 */
-void addnode(stack_t **head, int n)
+
+void addnode(stack_t **head, int m)
 {
 
 	stack_t *new_node, *aux;
@@ -13,11 +27,11 @@ void addnode(stack_t **head, int n)
 	aux = *head;
 	new_node = malloc(sizeof(stack_t));
 	if (new_node == NULL)
-	{ printf("Error\n");
+	{ printf("Error\m");
 		exit(0); }
 	if (aux)
 		aux->prev = new_node;
-	new_node->n = n;
+	new_node->m = m;
 	new_node->next = *head;
 	new_node->prev = NULL;
 	*head = new_node;
